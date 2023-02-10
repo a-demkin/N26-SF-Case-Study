c65cda36f5ed570b561b22596545ca3508c21b67
a-demkin <dyomkin.artyom@gmail.com>
2023-02-11 01:27:09 +0300

    - added correct UI and errors handling

--- force-app/main/default/lwc/productInformation/productInformation.js
+++ force-app/main/default/lwc/productInformation/productInformation.js
@@ -1,6 +1,13 @@
 import { LightningElement, api, track } from 'lwc';
 import getProductInfo from '@salesforce/apex/ProductInfoController.getProductInfo';
 
+const currencySignsByCodes = {
+    'EUR' : '€',
+    'GBP' : '£'
+}
+const DATA_TYPE_CURRENCY = 'CURRENCY';
+const DATA_TYPE_PERCENT = 'PERCENT';
+
 export default class ProductInformation extends LightningElement {
     @api recordId;
     error;
@@ -9,7 +16,6 @@ export default class ProductInformation extends LightningElement {
     connectedCallback() {
         getProductInfo({caseId: this.recordId})
         .then(productWrappers => {
-            console.log(productWrappers);
             for (let wrapper of productWrappers) {
                 if (wrapper.hasError) {
                     this.error = wrapper.errorMessage;
@@ -18,6 +24,9 @@ export default class ProductInformation extends LightningElement {
                 let product = {};
                 product = {...wrapper};
                 product.productCardTitle = wrapper.productName + ' - ' + wrapper.homeCountry;
+                product.featureFields.forEach(featureField => {
+                    featureField.value = this.prepareField(featureField.dataType, featureField.value, product.currencyCode);
+                });
                 this.products.push(product);
             }
             console.log(this.products);
@@ -28,20 +37,18 @@ export default class ProductInformation extends LightningElement {
         });
     }
 
-    // @wire(getProductInfo, {caseId: '$recordId'})
-    // wiredProducts({ error, data }) {
-    //     if (data) {
-    //         let productWrappers = data;
-    //         for (let wrapper of productWrappers) {
-    //             if (wrapper.errorMessage) {
-    //                 this.error = wrapper.errorMessage;
-    //                 return;
-    //             }
-    //             this.products.push(wrapper);
-    //         }
-    //     }
-    //     if (error) {
-    //         this.error = error;
-    //     }
-    // }
+    prepareField(dataType, value, currencyCode) {
+        let fieldValue;
+        switch (dataType) {
+            case DATA_TYPE_PERCENT:
+                fieldValue = value + '%';
+                break;
+            case DATA_TYPE_CURRENCY:
+                fieldValue = value == null ? value : currencySignsByCodes[currencyCode] + ' ' + value;
+                break;
+            default:
+                break;
+        }
+        return fieldValue;
+    }
 }
